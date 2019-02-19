21a7badff64bc973fdc7be94df57eb07dd405417
Daniil Blagiy <blagiy.daniil@gmail.com>
2019-02-19 16:25:33 +0300

    working on update time handling

--- DuggooEngine/src/duggoo/app/application.cpp
+++ DuggooEngine/src/duggoo/app/application.cpp
@@ -1,5 +1,9 @@
 #include "application.h"
 
+#include <chrono>
+
+#include <iostream>
+
 namespace dg { namespace app {
 
 	void Application::start()
@@ -8,11 +12,35 @@ namespace dg { namespace app {
 
 		onInit();
 
+		auto startTime = std::chrono::steady_clock::now();
+		auto startSec = std::chrono::steady_clock::now();
+
+		int frames = 0;
+
 		while (running)
 		{
 			// handle time
+			auto endTime = std::chrono::steady_clock::now();
+			auto endSec = std::chrono::steady_clock::now();
+			std::chrono::duration<double, std::milli> elapsedTime = endTime - startTime;
+			std::chrono::duration<double, std::milli> elapsedTimeSec = endSec - startSec;
+			//startTime = std::chrono::steady_clock::now();
+
+			if (elapsedTimeSec.count() >= 1000.0)
+			{
+				frames = 0;
+				startSec = std::chrono::steady_clock::now();
+			}
+			
+			// 67 frames per second
+			if (elapsedTime.count() < 15.0)
+				continue;
+
+			startTime = std::chrono::steady_clock::now();
+			frames++;
 			
 			onUpdate();
+			std::cout << frames << std::endl;
 		}
 
 		onDestroy();
