742ef334fdfb419538c3b12ff90b7a69620c2ed8
Daniil Blagiy <blagiy.daniil@gmail.com>
2019-03-31 21:43:41 +0300

    added renderable2d

---
+++ DuggooEngine/src/duggoo/graphics/renderable2d.h
@@ -0,0 +1,59 @@
+#pragma once
+
+#include "buffers/platform/gl_buffer.h"
+#include "buffers/platform/gl_indexbuffer.h"
+#include "buffers/platform/gl_vertex_array.h"
+#include "../math/vector.h"
+
+namespace dg { namespace graphics {
+
+	class Renderable2D
+	{
+	protected:
+		math::Vector3 position;
+		math::Vector2 size;
+		math::Vector4 color;
+
+		VertexArray* vertexArray;
+		IndexBuffer* indexBuffer;
+
+	protected:
+		Renderable2D(math::Vector3 s_position, math::Vector2 s_size, math::Vector4 s_color)
+			: position(s_position), size(s_size), color(s_color)
+		{
+			vertexArray = new VertexArray();
+			GLfloat vertices[] = 
+			{
+				0, 0, 0,
+				0, position.y, 0,
+				position.x, position.y, 0,
+				position.x, 0, 0
+			};
+
+			GLfloat colors[] =
+			{
+				color.x, color.y, color.z, color.w,
+				color.x, color.y, color.z, color.w,
+				color.x, color.y, color.z, color.w,
+				color.x, color.y, color.z, color.w
+			};
+
+			vertexArray->addBuffer(new Buffer(vertices, 4 * 3, 3), 0);
+			vertexArray->addBuffer(new Buffer(colors, 4 * 4, 4), 1);
+			
+			GLushort indices[] = { 0, 1, 2, 2, 3, 0 };		// A triangle
+			indexBuffer = new IndexBuffer(indices, 6);
+		}
+
+		virtual ~Renderable2D()
+		{
+			delete vertexArray;
+			delete indexBuffer;
+		}
+
+		math::Vector3 getPosition() { return position; };
+		math::Vector2 getSize() { return size; };
+		math::Vector4 getColor() { return color; };
+	};
+
+} }
